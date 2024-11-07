a83c5f560b9076461b5a97381a1957a0e7fe2fa6
Charles Siffrein-Blanc <chsiffre@z3r4p2.42lyon.fr>
2024-11-07 16:14:59 +0100

    ok

--- backend/accounts/models.py
+++ backend/accounts/models.py
@@ -1,3 +1,40 @@
+# from django.contrib.auth.models import AbstractUser
+# from django.db import models
+# import random
+# import string
+
+# class CustomUser(AbstractUser):
+#     # Vos champs existants
+#     is_2fa_enabled = models.BooleanField(default=False)
+#     two_factor_code = models.CharField(max_length=6, null=True, blank=True)
+#     two_factor_code_timestamp = models.DateTimeField(null=True, blank=True)
+
+#     avatar = models.ImageField(upload_to='avatars/', max_length=255, default='assets/avatars/ladybug.png')
+#     email = models.EmailField(unique=True)
+#     intra_42_id = models.IntegerField(null=True, blank=True, unique=True)
+#     is_42_user = models.BooleanField(default=False)
+#     groups = models.ManyToManyField(
+#         'auth.Group',
+#         related_name='customuser_set',
+#         blank=True
+#     )
+#     user_permissions = models.ManyToManyField(
+#         'auth.Permission',
+#         related_name='customuser_permissions_set',
+#         blank=True
+#     )
+
+#     def generate_2fa_code(self):
+#         """Génère un code 2FA à 6 chiffres"""
+#         code = ''.join(random.choices(string.digits, k=6))
+#         return code
+
+#     class Meta:
+#         db_table = 'accounts_customuser'
+
+####################################################
+
+
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 import random
@@ -13,6 +50,11 @@ class CustomUser(AbstractUser):
     email = models.EmailField(unique=True)
     intra_42_id = models.IntegerField(null=True, blank=True, unique=True)
     is_42_user = models.BooleanField(default=False)
+    # Nouveaux champs pour 2FA
+    is_2fa_enabled = models.BooleanField(default=False)
+    two_factor_code = models.CharField(max_length=6, null=True, blank=True)
+    two_factor_code_timestamp = models.DateTimeField(null=True, blank=True)
+
     groups = models.ManyToManyField(
         'auth.Group',
         related_name='customuser_set',
